f00cc6bf6e1b5199a32650bb03d8025ac4c5f8a8
Egor <ignatov.eo19@physics.msu.ru>
2025-05-03 15:18:24 +0300

    Поменял отображение интерфейса и границ мира

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Конфигурационные константы
 WORLD_SIZE = (800, 600)  # (width, height)
 UI_SETTINGS = {
-    'header_height': 60,
+    'header_height': 50,
     'border_width': 10,
     'fps': 60,
     'simulation_duration': 24000  # тиков на поколение
